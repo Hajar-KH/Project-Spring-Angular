b66ab2b35af43b4bb601d6ead6cb19937153821f
Hajar Khatiby <Hajar.Khatiby@emsi-edu.ma>
2024-01-17 13:50:32 +0100

    secondd

--- Backend/src/main/java/com/management/MainApplication.java
+++ Backend/src/main/java/com/management/MainApplication.java
@@ -3,7 +3,9 @@ package com.management;
 import com.management.entities.Admin;
 import com.management.entities.Role;
 
+import com.management.entities.Vehicule;
 import com.management.services.AdminService;
+import com.management.services.VehiculeService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -54,6 +56,23 @@ public class MainApplication {
 
     }
 
+    @Autowired
+    VehiculeService vehiculeService;
+    @PostConstruct
+    void initial(){
+        Vehicule v1=new Vehicule("citroen.png","Citroen C3","2018",220);
+        vehiculeService.saveVehicule(v1);
+        Vehicule v2=new Vehicule("peugeot.png","Peugeot 208","2021",200);
+        vehiculeService.saveVehicule(v2);
+        Vehicule v3=new Vehicule("principal.png","KIA Niro","2020",180);
+        vehiculeService.saveVehicule(v3);
+        Vehicule v4=new Vehicule("dacia.png","Dacia Duster","2019",350);
+        vehiculeService.saveVehicule(v4);
+        Vehicule v5=new Vehicule("fiat.png","Fiat 500","2023",200);
+        vehiculeService.saveVehicule(v5);
+        Vehicule v6=new Vehicule("clio.png","Megane","2018",250);
+        vehiculeService.saveVehicule(v6);
+    }
 
         }
 
--- Backend/src/main/java/com/management/entities/Admin.java
+++ Backend/src/main/java/com/management/entities/Admin.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @Entity
 //generer getters et setters
-
 @Data
 //generer constructeur
 @AllArgsConstructor
